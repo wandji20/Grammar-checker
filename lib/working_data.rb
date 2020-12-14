cac47ca6575c3feb858a18ccc6f831806bc990cd
wandji20 <wbertrand10@gmail.com>
2020-12-14 10:40:47 +0100

    create workdata class and get it to read an input text file

--- lib/working_data.rb
+++ lib/working_data.rb
@@ -2,10 +2,10 @@ require 'strscan'
 
 class WorkData
   attr_accessor :arr
+
   def initialize
     @arr = []
-    @my_file =  File.read('../user.txt')
+    @my_file = File.read('../user.txt')
     @working_file = StringScanner.new(@my_file)
   end
-
 end
